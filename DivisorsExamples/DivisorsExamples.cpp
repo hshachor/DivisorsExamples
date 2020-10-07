576116e5d598213f2bc7a28a925e6b3900d8de9f
Haim Shachor <hshachor@g.jct.ac.il>
2020-10-07 20:40:54 +0300

    Create first version to display in class (with obvious bug)

--- DivisorsExamples/DivisorsExamples.cpp
+++ DivisorsExamples/DivisorsExamples.cpp
@@ -1,20 +1,19 @@
-// DivisorsExamples.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter a number!\n";
+    unsigned int num, divisor = 2;
+    std::cin >> num;
+    std::cout << num << " = ";
+    while (num > 1) {
+        if (num % divisor == 0) {
+            num /= divisor;
+            std::cout << divisor << " * ";
+        }
+        else {
+            divisor++;
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
